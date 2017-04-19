0a15f7c49d41f27c1454cf6635c54386a55cf571
Eddy Drew <eddy@xcap.ca>
2017-04-19 00:39:59 +0000

    workshop one complete

--- index.js
+++ index.js
@@ -1,9 +1,152 @@
+"use strict";
+
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//Connecting to the database
+var mysql = require('promise-mysql');
+
+var connection = mysql.createPool({
+    host     : 'localhost',
+    user     : 'rockwell421', 
+    password : '',
+    database: 'reddit',
+    connectionLimit: 10
+    
 });
+
+var RedditAPI = require('./reddit');
+
+var myReddit = new RedditAPI(connection);
+
+app.set('view engine', 'pug');
+
+
+//Exercise-1
+
+app.get('/', function (request, response) {
+  response.send('<h1>Hello World!</h1>');
+});
+
+
+//Exercise-2
+
+app.get('/hello', function(request, response) {
+    
+    if (request.query.firstName) {
+        response.send("<h1>Hello " + request.query.firstName + "</h1>");
+    }
+    else {
+        response.send("<h1>Hello World!</h1>");
+    }
+});
+
+
+//Exercise-3
+
+app.get('/calculator/:operation', function (req, res) {
+
+//create two variables to assign the requests, making sure that they're numbers and not strings using Number constructor 
+  var num1 = Number(req.query.num1);
+  var num2 = Number(req.query.num2);
+
+//create if conditions that verify the operation type
+  if (req.params.operation === "add") {
+      res.send(`{
+        "operation": "add",
+        "firstOperand": ${num1},
+        "secondOperand": ${num2},
+        "solution": ${num1 + num2}
+     }`)
+    
+  } else if (req.params.operation === "multiply") {
+    
+      res.send(`{
+        "operation": "multiply",
+        "firstOperand": ${num1},
+        "secondOperand": ${num2},
+        "solution": ${num1 * num2}
+      }`)
+    } else {
+      res.status(400);
+  }
+  
+});
+
+/*
+test multiply
+http://week-2-mysql-data-rockwell421.c9users.io/calculator/multiply?num1=6&num2=4
+
+test sum
+http://week-2-mysql-data-rockwell421.c9users.io/calculator/sum?num1=6&num2=4
+*/
+
+
+//Exercise-4
+/* 
+Steps (connect API, create a promise)
+Connect the myReddit API file and chain it to getAllPosts as a promise
+Convert the array into a string using a map method and call posts title, posts url, and posts username
+Join the string
+*/
+
+app.get('/posts', (req, res) => {
+
+  myReddit.getAllPosts()
+    .then(allPosts => {
+      res.render('post-list', {allPosts: allPosts});
+      
+    });
+});
+    
+    
+    
+//Exercise 5
+
+app.get('/new-post', (request, response) => {
+  response.render('create-content');
+})
+
+
+
+//Exercise 6: Receiving Data From Our Form
+
+
+app.post('/createPost', (req, res) => {
+  myReddit.createPost({
+    url: req.body.url,
+    title: req.body.title,
+    subredditId: 325,
+    userId: 1667
+    
+  }).then(result => {
+      res.redirect("posts")
+    })
+  })
+    
+    
+    
+//Exercise 7: Using Pug
+
+app.get('/createContent', (req, res) => {
+  res.render('create-content');
+})
+
+//getAllPosts was modified for this exercise
+
+
+
+//Exercise 8
+
+
+
+
+
+
+
+
 
 
 
---
+++ reddit.js
@@ -0,0 +1,177 @@
+'use strict'
+var bcrypt = require('bcrypt-as-promised');
+var HASH_ROUNDS = 10;
+
+class RedditAPI {
+    constructor(conn) {
+        this.conn = conn;
+    }
+
+    createUser(user) {
+        return bcrypt.hash(user.password, HASH_ROUNDS)
+            .then(hashedPassword => {
+                return this.conn.query('INSERT INTO users (username,password, createdAt, updatedAt) VALUES (?, ?, NOW(), NOW())', [user.username, hashedPassword]);
+            })
+            .then(result => {
+                return result.insertId;
+            })
+            // .catch(error => {
+                // Special error handling for duplicate entry
+                // if (error.code === 'ER_DUP_ENTRY') {
+                //     throw new Error('A user with this username already exists');
+                // }
+                // else {
+                //     throw error;
+                // }
+            // });
+    }
+    
+    
+    createSubreddit(subreddit) {
+        // console.log(subreddit);
+        return this.conn.query(
+            `
+            INSERT INTO subreddits (name, description, createdAt, updatedAt)
+            VALUES (?, ?, NOW(), NOW())`, [subreddit.name, subreddit.description]
+            ) 
+            .then(result => {
+                return result.insertId;
+            })
+            // .catch(error => {
+                
+            //     if(error.code === 'ER_DUP_ENTRY') {
+            //         throw new Error('A subreddit with this name already exists! Try another topic :)')
+            //     } 
+            //     else {
+            //         throw error;
+            //     }
+            // });
+            ;
+    }
+
+
+//modify the createPost function to look for a subredditId property in the post 
+//object and use it. createPost should return an error if subredditId is not provided.
+
+//INSERT INTO posts (userId, title, url, createdAt, updatedAt)
+
+    createPost(post) {
+        return this.conn.query(
+            `INSERT INTO posts (userId, title, url, createdAt, updatedAt, subredditId) VALUES (?, ?, ?, NOW(), NOW(), ?)`,
+            [post.userId, post.title, post.url, post.subredditId])
+            .then(result => {
+                //console.log(result.subredditId);
+                return result.subredditId;
+            })
+            // .catch(error => {
+            //     // Special error handling for no subreddit ID
+            //     if (error.code === 'ER_NO_SRID') {
+            //         throw new Error('This Subreddit does not exist');
+            //     }
+            //     else {
+            //         throw error;
+            //     }
+            //     });
+    }
+
+
+    createVote(vote) {
+            if(vote.voteDirection === 1 || vote.voteDirection === 0 || vote.voteDirection === -1) {
+                return this.conn.query(
+                    `
+                    INSERT INTO votes SET postId = ?, userId = ?, voteDirection = ? 
+                    ON DUPLICATE KEY UPDATE voteDirection = ?
+                    `
+                    [vote.postId, vote.userId, vote.voteDirection, vote.voteDirection])
+                    .catch(console.log);
+            } 
+            else {
+                throw new Error('Bad Vote');
+            }
+        }
+
+
+
+    getAllPosts() {
+        return this.conn.query(
+            `
+            SELECT 
+                SUM(votes.voteDirection) AS votes_Score, 
+                posts.id AS posts_id, 
+                posts.title As posts_title, 
+                posts.url AS posts_url, 
+                posts.createdAt AS posts_createdAt, 
+                posts.updatedAt AS posts_updatedAt,
+                users.id AS users_id,
+                users.username AS users_username,
+                users.createdAt AS users_createdAt, 
+                users.updatedAt AS users_updatedAt,
+                subreddits.id AS subreddits_id,
+                subreddits.name AS subreddits_name,
+                subreddits.description AS subreddits_description,
+                subreddits.url AS subreddits_url,
+                subreddits.createdAt AS subreddits_createdAt,
+                subreddits.updatedAt AS subreddits_updatedAt
+            FROM posts 
+            JOIN users ON users.id = posts.userId
+            JOIN subreddits ON posts.subredditId = subreddits.id
+            LEFT JOIN votes ON posts.id = votes.postId
+            GROUP BY posts.id 
+            ORDER BY posts_createdAt DESC
+            LIMIT 25`
+      
+      //votes.postId      
+            
+        ).then (function(result) {
+            console.log(result)
+            return result.map(function(val) {
+                console.log(val);
+                    return {
+                        
+                        votes_Score: val.votes_Score,
+                        posts_id: val.posts_id,
+                        posts_title: val.posts_title,
+                        posts_url: val.posts_url,
+                        posts_createdAt: val.posts_createdAt,
+                        posts_updatedAt: val.posts_updatedAt,
+                        
+                        user:{
+                            users_id: val.users_id,
+                            users_username: val.users_username,
+                            users_createdAt: val.users_createdAt,
+                            users_updatedAt: val.users_updatedAt
+                        },
+                        
+                        subreddits:{
+                            subreddits_id: val.subreddits_id,
+                            subreddits_name: val.subreddits_name,
+                            subreddits_description: val.subreddits_description,
+                            subreddits_url: val.subreddits_url,
+                            subreddits_createdAt : val.subreddits_createdAt,
+                            subreddits_updatedAt: val.subreddits_updatedAt
+                        }
+                        
+                    }   
+                    //console.log('this is the result', val);
+            });
+        
+        });
+    }
+    
+    getAllSubreddits() {
+        return this.conn.query(
+            `
+            SELECT *
+            FROM subreddits
+            ORDER BY subreddits.createdAt DESC 
+            LIMIT 25
+            `
+            );
+    }
+    
+    
+}
+
+
+module.exports = RedditAPI;
+
